0ad86f60d3abe18b5a0b8b8f961f86df6eec8912
jialinli <jialinmli98@gmail.com>
2018-10-13 14:49:04 -0700

    code

--- shepherd/Code.py
+++ shepherd/Code.py
@@ -1,3 +1,4 @@
+import random
 class Code:
     def __init__(self, code, solution, perk):
         self.code = code
@@ -16,3 +17,28 @@ class Code:
             if c.code == code:
                 return c
         return 'No matching Code object by code'
+
+class Code:
+    codes = []
+    solutions = []
+    code_solution = {}
+    code_effect = {}
+    effect_list = list(Utils.EFFECT())
+    def all_codes():
+        list_of_code = []
+        for i in range(16):
+            new_code = generate_code(list_of_code)
+            list_of_code.append(newcode)
+        self.codes = list_of_code
+
+    def all_solutions(self, codes)
+        self.solutions = [decode(code) for code in codes]
+
+    def assign_code_solution(arg):
+        pass assign_code_solution():
+        for i in range(16):
+            self.code_solution[codes[i]] = self.solutions[i]
+
+    def assign_code_effect():
+        foe i in range(16):
+            self.code_effect[codes[i]] = self.effect[random.randint(0,len(effect_list))]
