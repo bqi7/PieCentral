1500cc6dec762c256f23afd45577f0fb98b30c34
Matthew Harrigan <betterthanbossfiredog5@gmail.com>
2019-01-23 20:09:10 -0800

    changed assign_code_solution to mutate rather than reassign global variables

--- shepherd/Code.py
+++ shepherd/Code.py
@@ -5,7 +5,6 @@ codes = []
 solutions = []
 code_solution = {}
 code_effect = {}
-effect_list = []
 effect_list = list(EFFECTS.ALL_EFFECTS)
 
 def generate_code(code_list):
@@ -20,18 +19,15 @@ def assign_code_solution():
     '''
     Generate 16 codes and create a code_solution dictionary
     '''
-    global codes
-    global solutions
-    global code_solution
-    global code_effect
-    global effect_list
     list_of_code = []
+    codes.empty()
+    solutions.empty()
     for i in range(16):
         # pylint: disable=assignment-from-no-return
-        new_code = generate_code(list_of_code)
-        list_of_code.append(new_code)
-    codes = list_of_code
-    solutions = [decode(code) for code in codes]
+        new_code = generate_code(codes)
+        codes.append(new_code)
+    for code in codes:
+        solutions.append(decode(code))
     for i in range(16):
         code_solution[codes[i]] = solutions[i]
     return code_solution
@@ -41,5 +37,5 @@ def assign_code_effect():
     Assign each code to a random effect
     '''
     for i in range(16):
-        code_effect[codes[i]] = effect_list[random.randint(0, len(effect_list))]
+        code_effect[codes[i]] = random.choice(effect_list)
     return code_effect
