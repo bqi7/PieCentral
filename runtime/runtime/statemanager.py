81095b89b7bb0dd9c1bb6e5b71dff12cf671287a
Jonathan Lee <jonathan-lee@berkeley.edu>
2019-01-27 01:42:45 -0800

    Finished async shared store

--- runtime/runtime/statemanager.py
+++ runtime/runtime/statemanager.py
@@ -3,6 +3,7 @@ import ctypes
 from functools import lru_cache, partial
 import enum
 
+import abc
 import asyncio
 import os
 import json
@@ -15,12 +16,13 @@ import yaml
 import queue
 import threading
 import uuid
+import pickle
 
 from numbers import Real
 from typing import List, Tuple
 
 from cobs import cobs
-from runtime.buffer import SharedMemoryBuffer
+from runtime.buffer import SharedMemoryBuffer, BinaryRingBuffer
 from runtime.util import read_conf_file, RuntimeException
 
 
@@ -100,125 +102,194 @@ class DeviceStructure(ctypes.Structure):
         pass
 
 
-class SharedStoreRequest(enum.Enum):
-    GET = enum.auto()
-    SET = enum.auto()
-    DEL = enum.auto()
+class SharedStore(UserDict, abc.ABC):
+    class Command(enum.Enum):
+        SET = enum.auto()
+        DEL = enum.auto()
 
+    delimeter = b'\x00'
 
-COBS_DELIMETER = '\x00'
-
-
-class SharedStoreServer(UserDict):
     def __init__(self, addr, data=None, name=None, daemon=True):
         self.addr, self.name, self.daemon = addr, name, daemon
-        self.ready = threading.Event()
         super().__init__(data or {})
 
     def start(self):
         self.thread = threading.Thread(name=self.name, daemon=self.daemon,
                                        target=self.bootstrap_thread)
         self.thread.start()
-        self.ready.wait()
+
+    @abc.abstractmethod
+    async def main(self):
+        pass
 
     def bootstrap_thread(self):
         try:
-            return asyncio.run(self.start_server())
+            self.loop = asyncio.new_event_loop()
+            asyncio.set_event_loop(self.loop)
+            self.main_task = self.loop.create_task(self.main())
+            self.loop.run_until_complete(self.main_task)
         except asyncio.CancelledError:
-            return
+            pass
+        finally:
+            tasks = asyncio.all_tasks(self.loop)
+            for task in tasks:
+                if not task.cancelled():
+                    self.loop.call_soon_threadsafe(task.cancel)
+            tasks = asyncio.gather(*tasks, return_exceptions=True)
+            self.loop.run_until_complete(tasks)
+            self.loop.run_until_complete(self.loop.shutdown_asyncgens())
+            self.loop.stop()
+            self.loop.close()
 
-    async def start_server(self):
+    def stop(self):
+        if hasattr(self, 'main_task') and not self.main_task.cancelled():
+            self.loop.call_soon_threadsafe(self.main_task.cancel)
+        if hasattr(self, 'thread'):
+            self.thread.join()
+
+    async def cobs_write(self, writer, data):
+        packet = cobs.encode(pickle.dumps(data))
+        writer.write(packet)
+        writer.write(self.delimeter)
+        await writer.drain()
+
+    async def cobs_read(self, reader):
+        packet = await reader.readuntil(self.delimeter)
+        return pickle.loads(cobs.decode(packet[:-len(self.delimeter)]))
+
+    async def recv_update(self, reader):
+        command, key, *value = data = await self.cobs_read(reader)
+        if command is SharedStore.Command.SET:
+            super().__setitem__(key, value[0])
+        elif command is SharedStore.Command.DEL:
+            super().__delitem__(key)
+        return data
+
+
+class SharedStoreServer(SharedStore):
+    def __init__(self, *args, buf_max_size=1024, **kwargs):
+        self.ready = threading.Event()
+        self.buf_lock, self.buf_max_size = asyncio.Lock(), buf_max_size
+        self.update_buffers = {}
+        super().__init__(*args, **kwargs)
+
+    def start(self):
+        super().start()
+        self.ready.wait()
+
+    async def main(self):
         self.server = await asyncio.start_unix_server(self.handle, self.addr)
-        self.ready.set()
         async with self.server:
+            self.ready.set()
             await self.server.serve_forever()
-
-    async def read(self, reader, buf):
-        pass
-
-    async def write(self, writer, buf):
-        pass
-
-    async def handle(self, reader, writer):
-        loop = self.server.get_loop()
-        # done, pending = await asyncio.wait(, loop=loop, return_when=asyncio.FIRST_COMPLETED)
-        """
-        print('Created!')
-        while True:
-            try:
-                packet = await reader.readuntil(COBS_DELIMETER)
-                decoded_data = cobs.decode(packet[:-len(COBS_DELIMETER)])
-                print(decoded_data)
-                await writer.drain()
-            except pickle.PickleError:
-                writer.write(cobs.encode(pickle.dumps()))
-                writer.write(COBS_DELIMETER)
-            except asyncio.IncompleteReadError:
-                break
-        """
 
     def stop(self):
         self.ready.wait()
         if self.server.is_serving():
-            self.server.close()
-            asyncio.run_coroutine_threadsafe(self.server.wait_closed(),
-                                             self.server.get_loop())
-            self.thread.join()
-            self.ready.clear()
+            try:
+                self.server.close()
+            except TypeError:  # HACK
+                pass
+        self.ready.clear()
+        super().stop()
+
+    async def read(self, reader, key):
+        while True:
+            try:
+                data = await self.recv_update(reader)
+                async with self.buf_lock:
+                    for update_key, buf in self.update_buffers.items():
+                        if update_key != key:
+                            await buf.put(data)
+            except pickle.PickleError:
+                continue
+
+    async def write(self, writer, key):
+        buf = self.update_buffers[key]
+        while True:
+            try:
+                await self.cobs_write(writer, await buf.get())
+            except pickle.PickleError:
+                continue
+
+    async def handle(self, reader, writer):
+        try:
+            key = str(uuid.uuid4())
+            async with self.buf_lock:
+                self.update_buffers[key] = asyncio.Queue(maxsize=self.buf_max_size)
+
+            tasks = {self.read(reader, key), self.write(writer, key)}
+            done, pending = await asyncio.wait(tasks, return_when=asyncio.FIRST_EXCEPTION)
+            for task in pending:
+                task.cancel()
+        finally:
+            writer.close()
+            async with self.buf_lock:
+                if key in self.update_buffers:
+                    del self.update_buffers[key]
+
+    async def send_set(self, key, value):
+        async with self.buf_lock:
+            for update_key, buf in self.update_buffers.items():
+                await buf.put((SharedStore.Command.SET, key, value))
+
+    async def send_del(self, key):
+        async with self.buf_lock:
+            for update_key, buf in self.update_buffers.items():
+                await buf.put((SharedStore.Command.DEL, key))
+
+    def __setitem__(self, key, value):
+        asyncio.run_coroutine_threadsafe(self.send_set(key, value), self.loop)
+        return super().__setitem__(key, value)
+
+    def __delitem__(self, key, value):
+        asyncio.run_coroutine_threadsafe(self.send_del(key), self.loop)
+        return super().__delitem__(key)
 
 
-class SharedStoreClient(UserDict):
-    def __init__(self, addr):
-        self.addr = addr
-        self.loop = asyncio.new_event_loop()
-        open_coro = asyncio.open_unix_connection(self.addr)
-        self.reader, self.writer = self.loop.run_until_complete(open_coro)
-        super().__init__()
+class SharedStoreClient(SharedStore):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
 
-    def close(self):
-        if not self.writer.is_closing() and not self.loop.is_closed():
+    async def main(self):
+        try:
+            reader, self.writer = await asyncio.open_unix_connection(self.addr)
+            while True:
+                try:
+                    await self.recv_update(reader)
+                except pickle.PickleError:
+                    continue
+        finally:
             self.writer.close()
-            self.loop.run_until_complete(self.writer.wait_closed())
-            self.loop.close()
-        else:
-            raise RuntimeException('Cannot close client more than once.')
 
-    def __getitem__(self, key):
-        pass
+    def __setitem__(self, key, value):
+        task = self.cobs_write(self.writer, (SharedStore.Command.SET, key, value))
+        asyncio.run_coroutine_threadsafe(task, self.loop)
+        return super().__setitem__(key, value)
+
+    def __delitem__(self, key):
+        task = self.cobs_write(self.writer, (SharedStore.Command.DEL, key))
+        asyncio.run_coroutine_threadsafe(task, self.loop)
+        return super().__delitem__(key)
 
 
+import time
 server = SharedStoreServer('./test')
 server.start()
 client = SharedStoreClient('./test')
-import time
-time.sleep(3)
-client.close()
+client.start()
+time.sleep(0.05)
+server['x'] = 1
+time.sleep(0.05)
+print(client['x'])
+client['x'] = 2
+time.sleep(0.05)
+print(server['x'])
+del client['x']
+time.sleep(0.1)
+assert 'x' not in server
+client.stop()
 server.stop()
-
-
-"""
-import time, random
-async def target(addr, wait):
-    # for _ in range(3):
-    reader, writer = await asyncio.open_unix_connection(addr)
-    await asyncio.sleep(2)
-    writer.write(cobs.encode(b'\x00\x01') + b'\x00')
-    await asyncio.sleep(wait)
-    writer.write(cobs.encode(b'\x01') + b'\x00')
-    writer.close()
-    await writer.wait_closed()
-    print('Closed client')
-bus = MessageBus()
-print('Bus initialized!')
-child1 = multiprocessing.Process(target=lambda addr: asyncio.run(target(addr, 2)), args=(bus.addr,))
-child1.start()
-child2 = multiprocessing.Process(target=lambda addr: asyncio.run(target(addr, 1)), args=(bus.addr,))
-child2.start()
-child1.join()
-child2.join()
-bus.close()
-# help(bus.server)
-"""
 
 
 
