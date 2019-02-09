b75fa2a16ee1e9e34e8d33f7d53927c41e556f1f
Andrew Wan <buzzbuzzonetwo@gmail.com>
2019-02-09 15:23:53 -0800

    help

--- shepherd/dawn_server.py
+++ shepherd/dawn_server.py
@@ -15,6 +15,10 @@ app = Flask(__name__)
 app.config['SECRET_KEY'] = 'omegalul!'
 socketio = SocketIO(app)
 
+@socketio.on('dawn-to-server-alliance-codes')
+def ui_to_server_setup_match(alliance_codes):
+    lcm_send(LCM_TARGETS.SHEPHERD, SHEPHERD_HEADER.CODE_APPLICATION, json.loads(alliance_codes))
+
 def receiver():
     events = gevent.queue.Queue()
     lcm_start_read(str.encode(LCM_TARGETS.DAWN), events, put_json=True)
@@ -26,6 +30,10 @@ def receiver():
             print("RECEIVED:", event)
             if eventDict["header"] == DAWN_HEADER.ROBOT_STATE:
                 socketio.emit(DAWN_HEADER.ROBOT_STATE, event)
+            if eventDict["header"] == DAWN_HEADER.CODES:
+                socketio.emit(DAWN_HEADER.CODES, event)
+            if eventDict["header"] == DAWN_HEADER.MASTER:
+                socketio.emit(DAWN_HEADER.MASTER, event)
             # else:
                 # socketio.emit(DAWN_HEADER.CODES, event)
         socketio.emit(DAWN_HEADER.HEARTBEAT, json.dumps({"heartbeat" : 1}))
