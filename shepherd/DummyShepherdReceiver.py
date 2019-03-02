1ff62884a3a4932f636f5a71efd601a3a920f2e3
Charles Hong <charleshong@berkeley.edu>
2019-03-02 15:12:42 -0800

    cleaned up code

--- shepherd/DummyShepherdReceiver.py
+++ shepherd/DummyShepherdReceiver.py
@@ -17,36 +17,35 @@ def receiver():
         #     x = {"RFID_list": s}
         #     lcm_send(LCM_TARGETS.UI, UI_HEADER.RFID_LIST, x)
         #     print("Sent RFIDs")
+        if overdrive:
+            lcm_send(LCM_TARGETS.SCOREBOARD, SCOREBOARD_HEADER.OVERDRIVE_START, {})
+            overdrive = False
         if event[0] == SHEPHERD_HEADER.GET_SCORES:
             x = {"blue_score": rand.randrange(100), "gold_score": rand.randrange(100)}
             lcm_send(LCM_TARGETS.UI, UI_HEADER.SCORES, x)
             print("Sent scores")
-        if event[0] == SHEPHERD_HEADER.SCORE_ADJUST:
+        elif event[0] == SHEPHERD_HEADER.SCORE_ADJUST:
             print(event[1])
-        if event[0] == SHEPHERD_HEADER.GET_MATCH_INFO:
+        elif event[0] == SHEPHERD_HEADER.GET_MATCH_INFO:
             x = {"match_num": rand.randrange(100), "b1name": "- a string1",
                  "b1num": rand.randrange(10), "b2name": "- a string2",
                  "b2num": rand.randrange(10), "g1name": "- a string3",
                  "g1num": rand.randrange(10), "g2name": "- a string4", "g2num": rand.randrange(10)}
             lcm_send(LCM_TARGETS.UI, UI_HEADER.TEAMS_INFO, x)
             print("Sent Team Info")
-        # if event[0] == SHEPHERD_HEADER.LAUNCH_BUTTON_TRIGGERED:
-        #     lcm_send(LCM_TARGETS.SCOREBOARD, SCOREBOARD_HEADER.LAUNCH_BUTTON_TIMER_START)
-        if event[0] == SHEPHERD_HEADER.SETUP_MATCH:
+        elif event[0] == SHEPHERD_HEADER.SETUP_MATCH:
             print(event[1])
-        if event[0] == SHEPHERD_HEADER.START_NEXT_STAGE:
+        elif event[0] == SHEPHERD_HEADER.START_NEXT_STAGE:
             print("starting stage")
-        if event[0] == SHEPHERD_HEADER.RESET_CURRENT_STAGE:
+        elif event[0] == SHEPHERD_HEADER.RESET_CURRENT_STAGE:
             print("reset stage")
-        if event[0] == SHEPHERD_HEADER.RESET_MATCH:
+        elif event[0] == SHEPHERD_HEADER.RESET_MATCH:
             print("reset match")
-        if overdrive:
-            lcm_send(LCM_TARGETS.SCOREBOARD, SCOREBOARD_HEADER.OVERDRIVE_START, {})
-            overdrive = False
         if event[0] == SHEPHERD_HEADER.LAUNCH_BUTTON_TRIGGERED:
             alliance = event[1]["alliance"]
             button = event[1]["button"]
-            lcm_send(LCM_TARGETS.SCOREBOARD, SCOREBOARD_HEADER.LAUNCH_BUTTON_TIMER_START, {"alliance": alliance, "button": button})
+            msg = {"alliance": alliance, "button": button}
+            lcm_send(LCM_TARGETS.SCOREBOARD, SCOREBOARD_HEADER.LAUNCH_BUTTON_TIMER_START, msg)
         if event[0] == SHEPHERD_HEADER.APPLY_PERKS:
             msg = {"alliance": event[1]["alliance"], "perk_1": event[1]["perk1"], "perk_2": event[1]["perk2"], "perk_3": event[1]["perk3"]}
             lcm_send(LCM_TARGETS.SCOREBOARD, SCOREBOARD_HEADER.PERKS_SELECTED, msg)
