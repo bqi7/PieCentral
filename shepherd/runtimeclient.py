64a385017791b2fe637c626460f801351f28cde5
Jonathan Lee <jonathan-lee@berkeley.edu>
2019-04-06 05:45:40 -0700

    runtimeclient update

--- shepherd/runtimeclient.py
+++ shepherd/runtimeclient.py
@@ -1,69 +1,44 @@
-import asyncio
-import aio_msgpack_rpc as rpc
-from functools import lru_cache
-import click
+import msgpackrpc
 
 
 class RuntimeClient:
-    """
-    A Python interface for communicating with Runtime.
-    This client uses `aio_msgpack_rpc` to perform asynchronous remote procedure
-    calls (RPC). The underlying messages are packaged using `msgpack`, a
-    JSON-like binary serialization format, so all parameters and return values
-    need to be serializable with `msgpack`.
-    At a low level, the client interacts with the server with either
-    notifications (one-way, non-blocking) or calls (request-response, blocking).
-    By default, the client uses calls, but notifications can be used with the
-    `block=False` keyword.
-    Example::
-        >>> async def main():
-        ...     client = RuntimeClient('192.168.128.200', 1234)
-        ...     async with client:
-        ...         await client.set_field_param('team', 'blue')
-        ...         print('Team:', await client.get_field_param('team'))
-        >>> asyncio.run(main())
-        Team: blue
-    References::
-        .. _aio-msgpack-rpc PyPI Page
-            https://pypi.org/project/aio-msgpack-rpc/
-    """
-    def __init__(self, host: str, port: int = 6200):
-        self.host, self.port = host, port
-        # Every client gets its own method wrapper cache.
-        self._get_method_wrapper = lru_cache(maxsize=1024)(self._get_method_wrapper)
+    def __init__(self, host, port):
+        self.host, self.port, self.client = host, port, None
 
-    async def __aenter__(self):
-        await self.connect()
-        return self
+    def connect(self):
+        self.client = msgpackrpc.Client(msgpackrpc.Address(self.host, self.port))
 
-    async def __aexit__(self, _exc_type, _exc, _tb):
-        await self.disconnect()
+    def disconnect(self):
+        self.client.close()
+        self.client = None
 
-    async def connect(self):
-        self.client = rpc.Client(*await asyncio.open_connection(self.host, self.port))
+    def set_mode(self, mode=True):
+        self.client.call('set_mode', mode)
 
-    async def disconnect(self):
-        if hasattr(self, 'client'):
-            self.client.close()
-            delattr(self, 'client')
+    def set_alliance(self, alliance):
+        self.client.call('set_alliance', alliance)
 
-    def _get_client(self):
-        try:
-            return self.client
-        except AttributeError as exc:
-            raise ValueError('Must connect to runtime before issuing commands.') from exc
+    def set_master(self, master):
+        self.client.call('set_master', master)
 
-    def _get_method_wrapper(self, name):
-        async def method_wrapper(*args, block=True, **kwargs):
-            client = self._get_client()
-            if block:
-                return await client.call(name, *args, **kwargs)
-            else:
-                client.notify(name, *args, **kwargs)
-        method_wrapper.__name__ = name
-        return method_wrapper
+    def set_starting_zone(self, zone):
+        self.client.call('set_starting_zone', zone)
 
-    def __getattr__(self, name):
-        if name in self.__dict__:
-            return self.__dict__[name]
-        return self._get_method_wrapper(name)
+    def run_challenge(self, seed, timeout=1):
+        self.client.notify('run_challenge', seed, timeout)
+
+    def get_challenge_solution(self):
+        return self.client.call('get_challenge_solution')
+
+
+class RuntimeClientManager:
+    def __init__(self, blue_alliance, gold_alliance):
+        self.blue_alliance, self.gold_alliance = blue_alliance, gold_alliance
+        self.clients = {
+            team: RuntimeClient(f'192.168.128.{200 + team}', 6020)
+            for team in self.blue_alliance + self.gold_alliance
+        }
+        for team in self.blue_alliance:
+            self.clients[team].set_alliance('blue')
+        for team in self.gold_alliance:
+            self.clients[team].set_alliance('gold')
