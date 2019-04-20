7d5b76029952742d92b3da2100d2dc9a4bcca572
Matthew Harrigan <betterthanbossfiredog5@gmail.com>
2019-04-20 01:44:33 -0700

    implemented slackbot in shepherd

--- shepherd/Shepherd.py
+++ shepherd/Shepherd.py
@@ -125,6 +125,13 @@ def to_setup(args):
 
 
 def to_perk_selection(args):
+    next_match_info = Sheet.get_match(match_number + 1)
+    b1name = next_match_info["b1name"]
+    b2name = next_match_info["b2name"]
+    g1name = next_match_info["g1name"]
+    g2name = next_match_info["g2name"]
+    bot.team_numbers_on_deck(b1name, b2name, g1name, g2name)
+    
     global game_state
     game_timer.start_timer(CONSTANTS.PERK_SELECTION_TIME + 2)
     game_state = STATE.PERK_SELCTION
---
+++ shepherd/bot.py
@@ -0,0 +1,25 @@
+import json
+import requests
+
+# Set the webhook_url to the one provided by Slack when you create the webhook at https://my.slack.com/services/new/incoming-webhook/
+webhook_url = 'https://hooks.slack.com/services/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'
+
+def notify_queueing(match_num):
+    send_plain_message("Match number "+str(match_num)+" is ending.")
+
+def team_numbers_on_deck(b1,b2,g1,g2):
+    send_plain_message("The following teams are now on deck: \n On the blue side\
+, we have team #%i, and team #%i \n On the gold side, we\
+ have team #%i, and team #%i" % (b1,b2,g1,g2))
+
+def send_plain_message(message):
+    slack_data = {'text': message}
+    response = requests.post(
+        webhook_url, data=json.dumps(slack_data),
+        headers={'Content-Type': 'application/json'}
+        )
+    if response.status_code != 200:
+        raise ValueError(
+            'Request to slack returned an error %s, the response is:\n%s'
+            % (response.status_code, response.text)
+        )
